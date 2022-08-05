0dbd5e83ec98bf4214fbc41aaa1c3fe8b9eff74c
tangpanqing <tangpanqing@qq.com>
2022-08-05 15:29:28 +0800

    init

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
+  github: 'https://github.com/tangpanqing/summerjs',
+  docsRepositoryBase: 'https://github.com/tangpanqing/summerjs-docs',
   titleSuffix: ' – SummerJS',
   logo: (
     <>
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="SummerJS: A Simple Backend Framework for JS/TS Developer" />
+      <meta name="og:description" content="SummerJS: A Simple Backend Framework for JS/TS Developer" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="twitter:site:domain" content="www.summerjs.net" />
+      <meta name="twitter:url" content="https://www.summerjs.net" />
+      <meta name="og:title" content="SummerJS: A Simple Backend Framework for JS/TS Developer" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="SummerJS" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © SummerJS.</>,
   unstable_faviconGlyph: '👋',
 }
